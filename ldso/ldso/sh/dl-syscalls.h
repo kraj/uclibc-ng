5ed651850a8be2e08e16d3c4dec3759d9aa3e47a
Waldemar Brodkorb <wbx@openadk.org>
2015-02-17 22:54:31 -0600

    workaround is no longer needed for gcc 4.8.x/gcc 4.9.x

--- ldso/ldso/sh/dl-syscalls.h
+++ ldso/ldso/sh/dl-syscalls.h
@@ -1,7 +1,1 @@
-#if __GNUC_PREREQ (4, 1)
-#warning !!! gcc 4.1 and later have problems with __always_inline so redefined as inline
-# ifdef __always_inline
-# undef __always_inline
-# define __always_inline __inline__
-# endif
-#endif
+/* stub for arch-specific syscall issues */
