910299c49f55e7954f1c5a4350faf0bd2cc6ae1c
Yoshinori Sato <ysato@users.sourceforge.jp>
2016-01-09 16:27:58 +0900

    h8300: O_DIRECT and O_DIRECTORY swapping.
    Different define of latest kernel.
    kernel define is
     #define O_DIRECT        00040000        /* direct disk access hint */
     #define O_DIRECTORY     00200000        /* must be a directory */
    
    Yoshinori Sato <ysato@users.sourceforge.jp>

--- libc/sysdeps/linux/h8300/bits/fcntl.h
+++ libc/sysdeps/linux/h8300/bits/fcntl.h
@@ -44,8 +44,8 @@
 #define O_ASYNC		 020000
 
 #ifdef __USE_GNU
-# define O_DIRECTORY	 040000	/* Must be a directory.	 */
-# define O_DIRECT	0200000	/* Direct disk access.	*/
+# define O_DIRECT	 040000	/* Direct disk access.	*/
+# define O_DIRECTORY	0200000	/* Must be a directory.	 */
 # define O_NOFOLLOW	0400000	/* Do not follow links.	 */
 # define O_NOATIME     01000000 /* Do not set atime.  */
 # define O_CLOEXEC     02000000 /* Set close_on_exec.  */
