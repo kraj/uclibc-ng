f5ff52245ff9f260ebdfc011315066446eeb729c
Waldemar Brodkorb <wbx@openadk.org>
2016-01-25 21:27:44 +0100

    add $ORIGIN test-case
    Signed-off-by: Leonid Lisovskiy <lly.dev@gmail.com>
    Signed-off-by: Waldemar Brodkorb <wbx@uclibc-ng.org>

---
+++ test/dlopen/tst-origin.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <dlfcn.h>
+
+#ifdef __UCLIBC__
+extern void _dlinfo(void);
+#endif
+
+int main(int argc, char **argv) {
+	void *h1, *h2;
+	int (*mydltest)(const char *s);
+	char *error;
+
+	h1 = dlopen ("libtest31.so", RTLD_LAZY);
+	if (!h1) {
+		fprintf(stderr, "Could not open libtest31.so: %s\n", dlerror());
+		exit(1);
+	}
+
+	h2 = dlopen ("libtest31.so", RTLD_NOLOAD);
+	if (!h2) {
+		fprintf(stderr, "Could not open libtest31.so(RTLD_NOLOAD): %s\n", dlerror());
+		exit(1);
+	}
+
+	mydltest = dlsym(h1, "dltest");
+	if ((error = dlerror()) != NULL)  {
+		fprintf(stderr, "Could not locate symbol 'dltest': %s\n", error);
+		exit(1);
+	}
+
+	dlclose(h2);
+	dlclose(h1);
+
+	return EXIT_SUCCESS;
+}
