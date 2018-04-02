161d237ad04eb3815ebf154f0bec9ab88195a70f
Waldemar Brodkorb <wbx@uclibc-ng.org>
2018-04-02 14:41:25 +0100

    use __NR_newfstatat only for modern Linux architectures
    Otherwise it breaks mips64 n64.
    Should be used for aarch64/tilegx only.

--- libc/sysdeps/linux/common/fstat.c
+++ libc/sysdeps/linux/common/fstat.c
@@ -20,7 +20,7 @@ int fstat(int fd, struct stat *buf)
 }
 libc_hidden_def(fstat)
 
-#elif __WORDSIZE == 64 && defined __NR_newfstatat
+#elif __WORDSIZE == 64 && defined __NR_newfstatat && !defined __ARCH_HAS_DEPRECATED_SYSCALLS__
 #include <fcntl.h>
 
 int fstat(int fd, struct stat *buf)
