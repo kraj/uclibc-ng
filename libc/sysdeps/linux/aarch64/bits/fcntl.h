9efce41dd3bb5d721a5b771931126f32a85e1958
Paul Cercueil <paul@crapouillou.net>
2021-02-18 22:23:19 +0000

    fcntl.h: Make F_DUPFD_CLOEXEC if _USE_XOPEN2K8
    The F_DUPFD_CLOEXEC flag was added in POSIX 2008.09.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- libc/sysdeps/linux/aarch64/bits/fcntl.h
+++ libc/sysdeps/linux/aarch64/bits/fcntl.h
@@ -105,9 +105,9 @@
 # define F_SETPIPE_SZ	1031	/* Set pipe page size array.  */
 # define F_GETPIPE_SZ	1032	/* Set pipe page size array.  */
 #endif
-#ifdef __USE_XOPEN2K8
+#if defined __USE_XOPEN2K8 || defined __USE_GNU
 # define F_DUPFD_CLOEXEC 1030	/* Duplicate file descriptor with
-				   close-on-exit set.  */
+				   close-on-exit set on new fd.  */
 #endif
 
 /* For F_[GET|SET]FD.  */
